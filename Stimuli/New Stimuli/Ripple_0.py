36fe180e9638c79048e4eca41536296a1884f4f7
lukerogerson <luke.e.rogerson@gmail.com>
2016-04-13 14:39:31 +0200

    Fixed ripple, initial parameterisation

--- Stimuli/New Stimuli/Ripple_0.py
+++ Stimuli/New Stimuli/Ripple_0.py
@@ -1,18 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on April 7th 2016 @author: Luke Edward Rogerson, AG Euler
+Created on Thu Apr  7 11:32:23 2016
 
-This stimulus is intended to be a natural expansion on the chirp. While the 
-chirp has several attractive properties (clearly identifies polarity, extracts
-sufficient frequency and contrast sensitivities for classification), it does 
-not provide a comprehensive exploration of the frequency and contrast parameter
-space.
-
-It is this latter problem which this stimulus seeks to resolve. The ripple 
-stimulus presents a sequence of sine waves, each governed by two parameters,
-one for frequency and another for contrast. The full parameter space is defined
-by a grid of possible parameter pairs, which are explored at random. The sine 
-wave is presented for a short time for each pair (approximately 3 seconds).
+@author: Luke
 """
 import collections
 from functools import partial
@@ -22,17 +12,21 @@ import random
 
 # Define global stimulus parameters
 p = {'_sName'          : "Ripple_0",
-     '_sDescr'         : "Sine wave ripples, varying frequency and contrast",
+     '_sDescr'         : "Sine wave ripple",
      'seed'            : 1,
      "nTrials"         : 1, 
-     "IHalf"           : 127,
-     "dxStim_um"       : 1000,   # Stimulus size
+     "iHalf"           : 127,
+     "iFull"           : 254,
+     "dxStim_um"       : 100,   # Stimulus size
      "durFr_s"         : 1/60.0, # Frame duration
-     'durRipple'       : 3,
+     'durRipple'       : 2.5,
      'contrast_max'    : 1,
+     'contrast_min'    : 0.2,
      'contrast_n'      : 5,
-     'frequency_max'   : 8,
+     'frequency_max'   : 6,
+     'frequency_min'   : 2,     
      'frequency_n'     : 10,
+     "tSteadyMID_s"    : 0.5,    # Light at 50% for steps
      }
      
 def buildStimulus(p):
@@ -41,9 +35,10 @@ def buildStimulus(p):
     Create single elliptical object, for which the freqeuency is modulated.'''
        
     random.seed(p['seed'])
-    contrast_values = np.linspace(p['contrast_max']/p['contrast_n'],p['contrast_max'],p['contrast_n'])
-    frequency_values = np.linspace(p['frequency_max']/p['frequency_n'],p['frequency_max'],p['frequency_n'])
-    p['contfreq_pairs'] = random.shuffle([(x,y) for x in frequency_values for y in contrast_values])
+    contrast_values = np.linspace(p['contrast_min'],p['contrast_max'],p['contrast_n'])
+    frequency_values = np.linspace(p['frequency_min'],p['frequency_max'],p['frequency_n'])
+    p['contfreq_pairs'] = [(x,y) for x in frequency_values for y in contrast_values]
+    random.shuffle(p['contfreq_pairs'])
     
     # Define stimulus objects
     QDS.DefObj_Ellipse(1, p["dxStim_um"], p["dxStim_um"])
@@ -54,15 +49,16 @@ def iterateStimulus(p):
     Iterate over all value pairs, calculating the intensity change at each
     timepoint. Render the intensity change of the ellipse.'''
     
-    ripple = lambda t, frequency, contrast: np.sin(np.pi*frequency*t)*contrast
-    rgb = lambda intensity, iHalf: 3*(int(intensity+iHalf),)
-    
     for (frequency,contrast) in p['contfreq_pairs']:
-        for t_pnt in np.linspace(0,p['durRipple'],p['durRipple']/p['durFr_s']):
-            RGB = [rgb(ripple(t_pnt,frequency,contrast),p['iHalf'])]
-            QDS.SetObjColor(1, [p["StimType"]], [RGB])
+        RGB = 3 *(p["iHalf"],)
+        QDS.SetObjColor (1, [1], [RGB])
+        QDS.Scene_Render(p["tSteadyMID_s"], 1, [1], [(0,0)], 0)
+        
+        for t_pnt in range(int(p['durRipple']/p['durFr_s'])):
+            Intensity = np.sin(np.pi*frequency*t_pnt)*contrast*p["iHalf"]+p["iHalf"]
+            RGB = 3 *(int(Intensity),) 
+            QDS.SetObjColor(1, [1], [RGB])
             QDS.Scene_Render(p["durFr_s"], 1, [1], [(0,0)], 0)
-
 # --------------------------------------------------------------------------
 dispatcher = collections.OrderedDict([
     ('init', partial(QDS.Initialize,p['_sName'],p['_sDescr'])),
@@ -75,4 +71,4 @@ dispatcher = collections.OrderedDict([
     ('stop', QDS.EndScript)]                               
 )
 
-# [dispatcher[process]() for process in list(dispatcher.keys())]
+[dispatcher[process]() for process in list(dispatcher.keys())]
